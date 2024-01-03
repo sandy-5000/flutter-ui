1b894467879d7b9a31187e5389996da117527703
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-01-03 21:15:30 +0530

    upload and added bui

---
+++ lib/pages/prompt_today.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+class PromptTodayPage extends StatefulWidget {
+  const PromptTodayPage({Key? key}) : super(key: key);
+
+  @override
+  _PromptPageState createState() => _PromptPageState();
+}
+
+class _PromptPageState extends State<PromptTodayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(30),
+                ),
+              ),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Find your',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Inspiration',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(244, 243, 243, 1),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black87,
+                        ),
+                        hintText: "Search you're looking for",
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: <Widget>[
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Promo Today',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        promoCard('assets/images/girl_1.jpg'),
+                        promoCard('assets/images/girl_2.png'),
+                        promoCard('assets/images/girl_3.jpg'),
+                        promoCard('assets/images/girl_4.jpg'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/images/girl_3.jpg'),
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          stops: const [0.3, 0.9],
+                          colors: [
+                            Colors.black.withOpacity(.8),
+                            Colors.black.withOpacity(.2),
+                          ],
+                        ),
+                      ),
+                      child: const Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Text(
+                            'Test Design',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget promoCard(String image) {
+  return AspectRatio(
+    aspectRatio: 2.62 / 3,
+    child: Container(
+      margin: const EdgeInsets.only(right: 15.0),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(image),
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: const [0.1, 0.9],
+            colors: [
+              Colors.black.withOpacity(.8),
+              Colors.black.withOpacity(.1),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
